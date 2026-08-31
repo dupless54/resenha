bee161d4d3b72c317fd28fbb6735a065455ce0a9
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 12:03:46 +0300

    CHORE: annotate room ban model

--- app/models/resenha/room_ban.rb
+++ app/models/resenha/room_ban.rb
@@ -11,3 +11,26 @@ module Resenha
     validates :user_id, uniqueness: { scope: :room_id }
   end
 end
+
+# == Schema Information
+#
+# Table name: resenha_room_bans
+#
+#  id           :bigint           not null, primary key
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  banned_by_id :bigint
+#  room_id      :bigint           not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  idx_resenha_room_bans_on_room_and_user       (room_id,user_id) UNIQUE
+#  index_resenha_room_bans_on_banned_by_id       (banned_by_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (banned_by_id => users.id) ON DELETE => nullify
+#  fk_rails_...  (room_id => resenha_rooms.id) ON DELETE => cascade
+#  fk_rails_...  (user_id => users.id) ON DELETE => cascade
+#
